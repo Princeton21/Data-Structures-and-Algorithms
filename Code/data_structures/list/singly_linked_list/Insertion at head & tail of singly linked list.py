9c900e6236c010392134232e077860258ffed36d
Pranav Singh <66460698+PranavSingh21@users.noreply.github.com>
2021-10-24 00:44:43 +0530

    Create Insertion at head & tail of singly linked list.py

---
+++ Code/data_structures/list/singly_linked_list/Insertion at head & tail of singly linked list.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+'''
+class Solution:
+    #Function to insert a node at the beginning of the linked list.
+    def insertAtBegining(self,head,x):
+        nn = Node(x)
+        nn.next = head
+        head = nn
+        return head
+    #Function to insert a node at the end of the linked list.
+    def insertAtEnd(self,head,x):
+        # code here
+        if head == None:
+            head = Node(x)
+            return head
+        curr = head
+        while curr.next!=None:
+            curr=curr.next
+        curr.next = Node(x)
+        return head
+
+#{ 
+#  Driver Code Starts
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+
+def printList(head):
+    while head:
+        print(head.data,end=' ')
+        head=head.next
+    print()
+
+if __name__ == '__main__':
+    t=int(input())
+    for cases in range(t):
+        n=int(input())
+        a=LinkedList()
+        
+        nodes_info=list(map(int,input().split()))
+        for i in range(0,len(nodes_info)-1,2):
+            if(nodes_info[i+1]==0):
+                a.head = Solution().insertAtBegining(a.head,nodes_info[i])
+            else:
+                a.head = Solution().insertAtEnd(a.head,nodes_info[i])
+        printList(a.head)
+
+ 
+# } Driver Code Ends
