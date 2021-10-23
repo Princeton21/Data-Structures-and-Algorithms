e0e49adc09859abf3a93a29abbdc9e2e7903a60e
kgoyal000 <62390711+kgoyal000@users.noreply.github.com>
2021-10-24 00:38:22 +0530

    Shortened the code

--- Code/search_algorithms/binary_search/binarySearch.cpp
+++ Code/search_algorithms/binary_search/binarySearch.cpp
@@ -1,67 +1,30 @@
-#include <iostream>
-#include <stdlib.h>
-
 using namespace std;
-
-//Binary Search function
-int binarySearch(int arr[], int l, int r, int x)
+int main()
 {
-    if (r >= l)
+    int i, arr[10], num, first, last, middle;
+    cout<<"Enter 10 Elements (in ascending order): ";
+    for(i=0; i<10; i++)
+        cin>>arr[i];
+    cout<<"\nEnter Element to be Search: ";
+    cin>>num;
+    first = 0;
+    last = 9;
+    middle = (first+last)/2;
+    while(first <= last)
     {
-        int mid = l + (r - l) / 2;
-
-        // If the element is present at the middle
-        if (arr[mid] == x)
-            return mid;
-
-        // If element is smaller than mid, then it can only be present in left subarray
-        if (arr[mid] > x)
-            return binarySearch(arr, l, mid - 1, x);
-
-        // Else the element can only be present in right subarray
-        return binarySearch(arr, mid + 1, r, x);
-    }
-
-    return -1;
-}
-
-int main(){
-    int n;
-    cout<<"Enter the size of array:";
-    cin>>n;
-    int *ptr = (int *)malloc(n * sizeof(int));
-    cout<<"Enter the array: ";
-    scanf("%d", ptr + 0);
-    //taking input along with sorting
-    for (int i = 1; i < n; i++)
-    {
-        scanf("%d", ptr + i);
-        for (int k = i; k > 0; k--)
+        if(arr[middle]<num)
+            first = middle+1;
+        else if(arr[middle]==num)
         {
-            if (ptr[k] < ptr[k - 1])
-            {
-                int t = ptr[k - 1];
-                ptr[k - 1] = ptr[k];
-                ptr[k] = t;
-            }
-            else
-            {
-                break;
-            }
+            cout<<"\nThe number, "<<num<<" found at Position "<<middle+1;
+            break;
         }
+        else
+            last = middle-1;
+        middle = (first+last)/2;
     }
-    int x;
-    cout<<"Enter the number that you want to find: ";
-    cin>>x;
-
-    int result = binarySearch(ptr, 0, n - 1, x);
-    if(result==-1){
-        cout<<"Number not found";
-    }
-    else{
-        cout << "Number found";
-    }
-    
-    
+    if(first>last)
+        cout<<"\nThe number, "<<num<<" is not found in given Array";
+    cout<<endl;
     return 0;
 }
