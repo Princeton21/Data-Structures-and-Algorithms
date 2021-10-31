9e555e643bb79d7f6915e5c1949564cbe802363a
anuraag1501 <69628853+anuraag1501@users.noreply.github.com>
2021-10-31 20:57:15 +0530

    Added Matrix Multiplication and spiral order
    Added Matrix Multiplication code and code for spiral order Matrix.

---
+++ Code/matrix/spiralOrderMatrix.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int m,n;
+    cin >> n >> m;
+    int arr[n][m];
+     
+     for(int i=0; i<n; i++){
+         for(int j=0; j<m; j++){
+             cin >> arr[i][j];
+         }
+     }
+
+     int row_start=0, row_end = n-1, col_start = 0, col_end = m-1;
+
+     while(row_start <= row_end && col_start <= col_end){
+
+        for(int col=col_start; col <= col_end; col++) cout << arr[row_start][col] <<" ";
+        row_start++;
+        
+        for(int row=row_start; row <= row_end; row++) cout << arr[row][col_end] <<" ";
+        col_end--;
+
+        for(int col=col_end; col>=col_start; col--) cout << arr[row_end][col] <<" ";
+        row_end--;
+
+        for(int row=row_end; row >=row_start; row--) cout << arr[row][col_start] << " ";
+        col_start++;
+        }
+    return 0;
+}
