3e5fff392bf5a8529b73acea810ab4e1e12f3e2b
MOUSSA Mohammed <mohamed.moussa@univ-mosta.dz>
2021-10-31 16:23:39 +0100

    Create linklist_node.js

---
+++ Code/data_structures/list/linklist_node.js
@@ -0,0 +1,19 @@
+// linkedlist class
+class LinkedList {
+    constructor()
+    {
+        this.head = null;
+        this.size = 0;
+    }
+  
+    // functions to be implemented
+    // add(element)
+    // insertAt(element, location)
+    // removeFrom(location)
+    // removeElement(element)
+  
+    // Helper Methods
+    // isEmpty
+    // size_Of_List
+    // PrintList
+}
